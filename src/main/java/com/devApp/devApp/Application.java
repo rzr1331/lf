ee7886c173c7b9469628e369a61c1849a49b720a
rzr1331 <rzr1331@github.com>
2020-02-08 17:41:35 +0530

    disabled cors filtering

--- src/main/java/com/devApp/devApp/Application.java
+++ src/main/java/com/devApp/devApp/Application.java
@@ -1,10 +1,17 @@
 package com.devApp.devApp;
 
 import com.devApp.devApp.service.AccountService;
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -14,6 +21,16 @@ public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 
 	@Override
